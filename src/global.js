067d8881e9efdbe1ad2daa2162d0b856f72f4d8e
Vaishak-JN <vaishakjnair.98@gmail.com>
2022-02-07 16:38:08 +0530

    .

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
-// export const API = "https://vrentalapp.herokuapp.com"
-export const API = "http://localhost:9000"
+export const API = "https://vrentalapp.herokuapp.com"
+// export const API = "http://localhost:9000"
